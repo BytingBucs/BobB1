f53b4fff51dc0fc9c030db1c58b5c114be020c54
Bobby Billings <Bobby Billings>
2016-02-18 01:21:49 -0500

    3rd commit. Many issues with author's code

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,13 +1,28 @@
 var main = function () {
 "use strict";
-
 $(".comment-input button").on("click", function (event) {
-
 var $new_comment = $("<p>"),
-comment_text = $(".comment-input input").val();
 
+comment_text = $(".comment-input input").val();
 $new_comment.text(comment_text);
 $(".comments").append($new_comment);
 });
 };
 $(document).ready(main);
+
+//the following is the authors code to fix the bug
+//It does not work. Typed it in, then copy/pasted
+//it still does not work. In fact, it causes
+//the page to no longer output input text:
+/*
+var main = function () {
+"use strict";
+$(".comment-input button").on("click", function (event) {
+var $new_comment;
+if ($(".comment-input input").val() !== "") {
+$new_comment = $("<p>").text($(".comment-input input").val());
+$(".comments").append($new_comment);
+}
+});
+$(document).ready(main);
+*/
